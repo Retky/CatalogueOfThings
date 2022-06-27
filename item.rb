63df76b8d135a5ccb5f8595a0b40ff244e506d19
@TesheMaximillan <dear.teshe@gmail.com>
2022-06-28 00:51:56 +0300

    Create archive methods

--- item.rb
+++ item.rb
@@ -13,4 +13,15 @@ class Item
     @genre = genre
     @archived = false
   end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  private
+
+  def can_be_archived?
+    # TODO: implement better check
+    (Date.today.year - @publish_date.year) > 10
+  end
 end
