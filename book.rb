547cd89d57222c210ef595118b1e129e51ca3df0
Teshome Kurabachew <dear.teshe@gmail.com>
2022-06-28 21:54:19 +0300

    Fix linter

--- book.rb
+++ book.rb
@@ -4,10 +4,11 @@ require 'date'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher:, cover_state:, publish_date:, author:, label:, source:, genre:)
+  def initialize(publisher:, cover_state:, publish_date:, author:, **options)
     @publisher = publisher
     @cover_state = cover_state
-    super(publish_date: publish_date, author: author, label: label, source: source, genre: genre)
+    super(publish_date: publish_date, author: author, label: options[:label],
+          source: options[:source], genre: options[:genre])
   end
 
   private
