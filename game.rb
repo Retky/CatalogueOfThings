3c135f79ae571fa93c1de07a87fb5407ae527f34
Henry Nawrocki <hdev@sztanski.com>
2022-06-29 08:59:37 -0500

    Add tests for game class methods

--- game.rb
+++ game.rb
@@ -1,8 +1,12 @@
-require './item'
+require_relative 'item'
 
-class Game
-  def initialize(params = {})
-    super(params[:publish_date], params[:author], params[:label], params[:source], params[:genre])
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(params)
+    super(publish_date: params[:publish_date], author: params[:author], label: params[:label],
+          source: params[:source], genre: params[:genre])
+
     @multiplayer = params[:multiplayer]
     @last_played_at = params[:last_played_at]
   end
